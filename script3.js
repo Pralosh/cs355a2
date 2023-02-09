fd26aa92082d868629cf1d1b04131e1bc099c3f3
pralosh <praloshrajbhandari@gmail.com>
2023-02-09 11:15:20 -0500

    script3 2nd version

--- script3.js
+++ script3.js
@@ -5,22 +5,28 @@ let currentIndex = 3;
 function changeSlide(x) {
     
     if(x == 2) {
-        let i = 0;
-        while(i < len) {
-            slides[i].style.zIndex = "0";
-            i++;
-        }
+        // let i = 0;
+        // while(i < len) {
+        //     slides[i].style.zIndex = "0";
+        //     i++;
+        // }
 
-        if(currentIndex == len) {
-            currentIndex = 0;
-            console.log("current index" + currentIndex);
-            changeSlide(2);
-        }
-        else if(currentIndex < len) {
-            slides[currentIndex].style.zIndex = "1";
-            currentIndex++;
-            console.log("current index" + currentIndex);
-        }
+        // if(currentIndex == len) {
+        //     currentIndex = 0;
+        //     console.log("current index" + currentIndex);
+        //     changeSlide(2);
+        // }
+        // else if(currentIndex < len) {
+        //     slides[currentIndex].style.zIndex = "1";
+        //     currentIndex++;
+        //     console.log("current index" + currentIndex);
+        // }
+        slides[currentIndex].style.zIndex = "0";
+        currentIndex++;
+        currentIndex=currentIndex%len;
+        console.log("current index" + currentIndex);
+        slides[currentIndex].style.zIndex = "1";
+
     }
 
     if(x == 1) {
